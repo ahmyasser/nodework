1d553e692a65887c9625f90684a73ee228eac1b3
ahmyasser <ahmedtricker@gmail.com>
2019-07-01 19:30:22 +0200

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs =require('fs');
 
-var port = 3000 || process.env.PORT  ;
+var port =  process.env.PORT || 3000 ;
 
 var app = express();
 app.use(express.static( __dirname + '/public'));
